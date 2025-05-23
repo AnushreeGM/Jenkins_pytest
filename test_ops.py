dd53b71d5ad275a4b741dc95215f938bb6401baa
AnushreeGM <117091147+AnushreeGM@users.noreply.github.com>
2025-05-23 09:28:58 +0530

    Create test_ops.py

---
+++ test_ops.py
@@ -0,0 +1,9 @@
+from ops import *
+def test_add():
+  assert add(2,3) == 5
+def test_sub():
+  assert sub(5,3) == 2
+def test_mul():
+  assert mul(2,3) == 6
+def test_divide():
+  assert div(10,5) == 2
